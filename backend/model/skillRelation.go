b6079766a4f9f38591360a58920c8b197e01a498
DigitalDemi <100835583+DigitalDemi@users.noreply.github.com>
2025-02-13 17:38:17 +0000

    Feat: Reorganising project

--- backend/model/skillRelation.go
+++
@@ -1,9 +0,0 @@
-package model
-
-
-type SkillRelation struct {
-    SourceSkillID int     `json:"source_skill_id"`   // The skill from which the relation starts
-    TargetSkillID int     `json:"target_skill_id"`   // The skill to which the relation points
-    RelationType  string  `json:"relation_type"`     // Type of relation ("Related To", "Prerequisite", etc.)
-}
-
